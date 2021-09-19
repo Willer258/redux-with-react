f0fad6f9087ff19a2bd807276154919caa85d944
Wilfried <wilfriedhouinlindjonon91@gmail.com>
2021-09-19 08:33:31 +0000

    Connect HOC

--- src/components/phone-component.js
+++ src/components/phone-component.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 import phone from "../image/image.jpg";
-function PhoneComponent() {
+function PhoneComponent(props) {
   return (
     <div className="p-5 text-center">
       <img src={phone} alt="phone" />
       <p className="p-5">
-        Dispos: <span></span>
+        Dispos: <span>{props.phones}</span>
       </p>
-      <button class="p-2 bg-green-200 hover:bg-green-500">Acheter</button>
+      <button className="p-2 bg-green-200 hover:bg-green-500">Acheter</button>
     </div>
   );
 }
-export default PhoneComponent;
+
+
+//A partir du provider la fonction mapStatetoProps recupere notre state dans le reducer pour nous le livrer en props
+const mapStatetoProps = (state)=>{
+  return {
+    phones: state.phones  }
+}
+export default  connect(mapStatetoProps)(PhoneComponent);
+
+// export default  connect(mapStatetoProps)(PhoneComponent); permet grace a redux surpasser notre composant
--- src/redux/phone/type.js
+++ src/redux/phone/type.js
@@ -1,3 +1,3 @@
-// Declaration de tout les types 
+// Declaration de tout les types pour le composant phone
  
  export const BUY_PHONE = "BUY_PHONE"
